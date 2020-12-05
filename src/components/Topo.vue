2e6d1a6653a086d7b9e6632c6389dc393f436819
Magnus Froes <jabazin@gmail.com>
2020-12-05 00:48:33 -0300

    Terceiro commit

---
+++ src/components/Topo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Exercícios com Vue.js</h1>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+h1 {
+    font-family: "Norse", Helvetica, Arial;
+}
+
+</style>
