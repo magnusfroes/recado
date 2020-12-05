f96417282c929b517a4282100e7d11e61b4c9e54
Magnus Froes <jabazin@gmail.com>
2020-12-04 23:14:30 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Recado/>
+  </div>
+</template>
+
+<script>
+import Recado from './components/Recado'
+
+export default {
+  name: 'App',
+  components: {
+    Recado
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+@font-face {
+  font-family: "Norse";
+  src: local("Norse"),
+   url(./fonts/Norse.otf) format("truetype");
+}
+
+</style>
---
+++ src/components/Recado.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="tabela">
+    <div class="entrada">
+      <label for="nome">Nome:</label>
+      <input type="text" v-model="nome">
+      <br><br>
+      <label for="recado">Recado:</label>
+      <textarea cols="30" rows="10" v-model="recado"></textarea>
+      <br><br>
+      <label for="data">Data:</label>
+      <input type="text" v-model="data">
+    </div>
+    <div class="carta">
+      <h2>{{ nome }}</h2>
+      <p>{{ recado }}</p>
+      <br>
+      <p>{{ data }}</p>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        nome: '',
+        recado: '',
+        data: ''
+      }
+    }
+}
+</script>
+
+
+<style scoped>
+
+.entrada {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.tabela {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-around;
+  width: 100%;
+}
+.carta {
+  display: flex;
+  margin: 20px;
+  padding: 20px;
+  width: 300px;
+  height: 420px;
+  flex-direction: column;
+  word-wrap: break-word;
+  background-image: url('../assets/papel.jpg');
+  background-size: 100%;
+  background-repeat: no-repeat;
+  font-family: "Norse", Helvetica, Arial;
+  font-size: 18px;
+}
+</style>
